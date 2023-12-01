514b12af781996b17f7722b9b1d3fe8b30f0216d
Rafael Vargas <rafael.vargas@gmail.com>
2023-11-30 23:30:21 -0300

    [update] init Gin

--- cmd/goopportunities/main.go
+++ cmd/goopportunities/main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/RaffDv/goopportunities/internal/router"
+
 func main() {
+	router.Initialize()
 
 }
---
+++ internal/router/router.go
@@ -0,0 +1,10 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	r := gin.Default()
+
+	r.Run(":4000")
+
+}
